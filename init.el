f53433e699e0845a9fedfbd5a051b70e01f25d2c
Jakob Klemm <jakob.elia.klemm@gmail.com>
2020-08-12 00:09:27 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,13 @@
+;; File executes babel on the real config file config.org.
+;; Init package managers
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file "~/.emacs.d/config.org")
