252ca04aa42da30acc6b47459d019bde2231101f
Jakob Klemm <jakob.elia.klemm@gmail.com>
2020-08-28 19:05:40 +0200

    Added German autocorrect. Removed temporary files.

--- rodian/rodian.el
+++ rodian/rodian.el
@@ -5,10 +5,8 @@
 
 (org-link-set-parameters
  "md5"
- :follow (lambda (path) (shell-command (concat "echo " path)))
- :export (lambda (path desc backend)
-           (interactive "FFind file: ")
-           (shell-command-to-string (concat "echo " (buffer-file-name (find-file-noselect filename)))))
+ :follow (lambda (path) (md5-open path))
+ :export #'md5-store
  :face '(:foreground "yellow")
  :help-echo "Rodian cloud file system with md5 indexing.")
 
@@ -16,5 +14,24 @@
   "md5 stuff"
   (interactive "FFind file: ")
   (shell-command-to-string (concat "echo " (buffer-file-name (find-file-noselect filename)))))
+
+(defun md5-open (file)
+  (shell-command (concat "echo " file))
+  )
+
+(defun md5-store ()
+  "Store a link to a man page."
+  (let* (page (get-file ()))
+    (link (concat "md5:" page))
+    (description (format "Man page for %s" page))
+    (org-link-store-props
+     :type "md5"
+     :link link
+     :description description)))
+
+(defun get-file ()
+  (interactive "FFind file: ")
+  (concat "echo " (buffer-file-name (find-file-noselect filename)))
+  )
 
 (provide 'rodian)
