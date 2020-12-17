303109644e6de34517381eb888d72f8fc3627649
Jakob Klemm <jakob.klemm@protonmail.com>
2020-12-17 08:09:51 +0100

    Updated auto save and removed temporary files.

--- setup.el
+++ setup.el
@@ -5,3 +5,8 @@
 ;; All code added by "custom" is stored in "custom.el"
 (setq custom-file "~/.emacs.d/custom.el")
 (load-file "~/.emacs.d/custom.el")
+
+
+;; Auto save related settings
+(setq auto-save-file-name-transforms
+          `((".*" ,(concat user-emacs-directory "auto-save/") t))) 
