4d9fd8626e8b9edb123decfc4205314a8aad133d
Jakob Klemm <jakob.elia.klemm@gmail.com>
2020-12-23 02:55:14 +0100

    Restructured and moved files.

--- init.el
+++ init.el
@@ -29,8 +29,39 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-;; All config and module loading is handled by "setup.el"
-(load-file (concat user-emacs-directory "setup.el"))
+;; Config structure:
+;; setup.el -> Calls out to all required modules and loads them.
+;; custom.el -> Added by Custom.
+;; defaults.el -> Basic settings, disable & change emacs stuff.
+;; qol.el -> Quality of life changes, code snippets, etc. (mostly minor changes)
+;; development/major.el -> Entry point for programming major modes.
+;; org/org.el -> Entry point for org-mode config, has children.
+;; editor -> General editor settings and packages.
+;; design.el -> Configures theme, colors, etc.
+;; projects/projects.el -> Entry point for project related functions, has children.
+;; binds.el -> Setup evil and navigation functions (partially in qol.el)
+;; navigation.el -> Helm setup for files, buffers, MX-, etc.
+
+;; Store scripts and snippets in ressource/ directory
+(add-to-list 'load-path "~/.emacs.d/resources/")
+;; Initial settings, disable some emacs features.
+(load-file (concat user-emacs-directory "modules/defaults.el"))
+;; Quality of life changes
+(load-file (concat user-emacs-directory "modules/qol.el"))
+;; Design
+(load-file (concat user-emacs-directory "modules/design.el"))
+;; Binds
+(load-file (concat user-emacs-directory "modules/binds.el"))
+;; Navigation
+(load-file (concat user-emacs-directory "modules/navigation.el"))
+;; Editor
+(load-file (concat user-emacs-directory "modules/editor.el"))
+;; Projects
+(load-file (concat user-emacs-directory "modules/projects/projects.el"))
+;; Org-mode
+(load-file (concat user-emacs-directory "modules/org/org.el"))
+;; Major programming modes
+(load-file (concat user-emacs-directory "modules/development/major.el"))
 
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file 'noerror)
