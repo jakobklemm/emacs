4d9f1ab32d2195f12071b148fb52c035c4db2999
Jakob Klemm <github@jeykey.net>
2021-03-29 09:41:57 +0200

    New mini modeline, removed awesome-tray.

--- modules/design.el
+++ modules/design.el
@@ -81,5 +81,17 @@ Each function is called with window as its sole arguemnt, returning a non-nil va
   (minions-mode 1)
   )
 
-(require 'awesome-tray)
-(awesome-tray-mode 1)
+(use-package    feebleline
+  :ensure       t
+  :config       (setq feebleline-msg-functions
+                      '((feebleline-line-number         :post "" :fmt "%5s")
+                        (feebleline-column-number       :pre ":" :fmt "%-2s")
+                        (feebleline-file-directory      :face feebleline-dir-face :post "")
+                        (feebleline-file-or-buffer-name :face font-lock-keyword-face :post "")
+                        (feebleline-file-modified-star  :face font-lock-warning-face :post "")
+                        (feebleline-git-branch          :face feebleline-git-face :pre " : ")
+                        (feebleline-project-name        :align right)
+			((lambda () (format-time-string "[%H:%M:%S]")) :align right)
+			)
+		      )
+                (feebleline-mode 1))
