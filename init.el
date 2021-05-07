b5a48acc4b9d23325c527e80646af9334e1a087e
Jakob Klemm <github@jeykey.net>
2021-05-07 11:01:46 +0200

    Added basic navi & edit config

--- init.el
+++ init.el
@@ -1,35 +1,23 @@
 ;; Entry point for emacs config.
 
-(require 'package)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http:///orgmode.org/elpa/") t)
 
-(when (not (package-installed-p 'use-package))
-  (package-refresh-contents)
-  (package-install 'use-package)
-  )
-
-(setq use-package-always-ensure t
-      use-package-expand-minimally t
-      use-package-compute-statistics t
-      use-package-enable-imenu-support t
-      )
-
+(straight-use-package 'use-package)
 
 (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(perfect-margin use-package dracula-theme auto-package-update)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)
